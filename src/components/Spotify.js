3623cde5d084999efc17c34ce2180d0881d23b9f
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-11 11:05:47 +0530

    letc

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
 import getNowPlayingItem from "../APIs/SpotifyAPI";
 import ProgressBar from "@ramonak/react-progress-bar";
 
@@ -17,51 +18,73 @@ export function SpotifyPlayer(props) {
     });
   });
 
-  return (
-    <div className="nowplaying">
-      <div className="spotify-cont">
-        <div className="spotify-track">
-          {result.isPlaying ? (
-            <a href={result.songUrl} target="_blank" rel="noreferrer">
-              <div className="song-img">
-                <img
-                  src={result.albumImageUrl}
-                  alt={`Album cover for ${result.title}`}
-                />
-              </div>
-              <div className="song-info">
-                <div className="song-title">{result.title}</div>
-                <div className="song-artist">
-                  <small>
-                    by {result.artist} <br /> on {result.albumName}
-                  </small>
+  useEffect(() => {
+    const svgMarkup = renderToStaticMarkup(renderComponent());
+    console.log(svgMarkup);
+  });
+
+  const renderComponent = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="350"
+        height="200"
+        viewBox="0 0 350 200"
+      >
+        <foreignObject x="0" y="0" width="350" height="200">
+          <div xmlns="http://www.w3.org/1999/xhtml">
+            <div className="nowplaying">
+              <div className="spotify-cont">
+                <div className="spotify-track">
+                  {result.isPlaying ? (
+                    <a href={result.songUrl} target="_blank" rel="noreferrer">
+                      <div className="song-img">
+                        <img
+                          src={result.albumImageUrl}
+                          alt={`Album cover for ${result.title}`}
+                        />
+                      </div>
+                      <div className="song-info">
+                        <div className="song-title">{result.title}</div>
+                        <div className="song-artist">
+                          <small>
+                            by {result.artist} <br /> on {result.albumName}
+                          </small>
+                        </div>
+                        <ProgressBar
+                          completed={result.progressMs}
+                          maxCompleted={result.durationMs}
+                          bgColor="#dbb3fe"
+                          height="8px"
+                          labelColor="#dbb3fe"
+                          baseBgColor="#121212"
+                          transitionDuration="0s"
+                        />
+                      </div>
+                    </a>
+                  ) : (
+                    <div>
+                      <div className="song-img">
+                        <img
+                          src="https://user-images.githubusercontent.com/70943732/232979556-2a30490b-10ab-4da3-9d7d-359e0afa6b23.png"
+                          alt="music"
+                        />
+                      </div>
+                      <div className="song-info">
+                        <div className="song-artist">
+                          Not listening to Spotify rn!
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
-                <ProgressBar
-                  completed={result.progressMs}
-                  maxCompleted={result.durationMs}
-                  bgColor="#dbb3fe"
-                  height="8px"
-                  labelColor="#dbb3fe"
-                  baseBgColor="#121212"
-                  transitionDuration="0s"
-                />
-              </div>
-            </a>
-          ) : (
-            <div>
-              <div className="song-img">
-                <img
-                  src="https://user-images.githubusercontent.com/70943732/232979556-2a30490b-10ab-4da3-9d7d-359e0afa6b23.png"
-                  alt="music"
-                />
-              </div>
-              <div className="song-info">
-                <div className="song-artist">Not listening to Spotify rn!</div>
               </div>
             </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+          </div>
+        </foreignObject>
+      </svg>
+    );
+  };
+
+  return <div>{renderComponent()}</div>;
 }
