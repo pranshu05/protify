c5edc52c55f01278f0c865033f9d7d35c816547e
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-11 10:06:19 +0530

    first commit?

---
+++ src/components/Spotify.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import getNowPlayingItem from "../APIs/SpotifyAPI";
+import ProgressBar from "@ramonak/react-progress-bar";
+
+export function SpotifyPlayer(props) {
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    Promise.all([
+      getNowPlayingItem(
+        props.client_id,
+        props.client_secret,
+        props.refresh_token
+      ),
+    ]).then((results) => {
+      setResult(results[0]);
+    });
+  });
+
+  return result.isPlaying ? (
+    <svg viewBox="0 0 100 100">
+      <rect width="100%" height="100%" fill="white" />
+      <div className="nowplaying">
+        <h1>Listening Activity</h1>
+        <div className="spotify-cont">
+          <div className="spotify-track">
+            <a href={result.songUrl} target="_blank" rel="noreferrer">
+              <div className="song-img">
+                <img
+                  src={result.albumImageUrl}
+                  alt={`Album cover for ${result.title}`}
+                />
+              </div>
+              <div className="song-info">
+                <div className="song-title">{result.title}</div>
+                <div className="song-artist">
+                  <small>
+                    by {result.artist} <br /> on {result.albumName}
+                  </small>
+                </div>
+                <ProgressBar
+                  completed={result.progressMs}
+                  maxCompleted={result.durationMs}
+                  bgColor="#dbb3fe"
+                  height="8px"
+                  labelColor="#dbb3fe"
+                  baseBgColor="#121212"
+                  transitionDuration="0s"
+                />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </svg>
+  ) : (
+    <svg viewBox="0 0 100 100">
+      <rect width="100%" height="100%" fill="white" />
+      <div className="nowplaying">
+        <h1>Listening Activity</h1>
+        <div className="spotify-cont">
+          <div className="spotify-track">
+            <div className="song-img">
+              <img
+                src="https://user-images.githubusercontent.com/70943732/232979556-2a30490b-10ab-4da3-9d7d-359e0afa6b23.png"
+                alt="music"
+              />
+            </div>
+            <div className="song-info">
+              <div className="song-artist">Not listening to Spotify rn!</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </svg>
+  );
+}
