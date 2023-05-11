4f5b14939704af52d2cb8f2ad71c9f30f8311665
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-11 11:11:07 +0530

    pain :sob:

--- public/renderSvg.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
-import { SpotifyPlayer } from "../components/Spotify";
-
-export function renderSvg() {
-  const svgMarkup = renderToStaticMarkup(<SpotifyPlayer />);
-  return svgMarkup;
-}
